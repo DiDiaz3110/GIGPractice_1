f0dc76caa877d3809f9b202866efc7ea2f9eb061
DiDiaz3110 <myzhanym@gmail.com>
2021-04-24 16:52:38 -0400

    More words

--- src/day1/HelloWorld.java
+++ src/day1/HelloWorld.java
@@ -3,5 +3,7 @@ package day1;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Adding to GitHub now");
+        System.out.println("Yay stuff are in GitHub");
     }
 }
